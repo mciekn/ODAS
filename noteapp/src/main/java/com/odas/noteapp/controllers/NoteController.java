c3eebb9b03bfc2681be53631e54ebbe30d9d4fcc
Maciej Niszczota <macni.macni@gmail.com>
2022-12-16 23:45:57 +0100

    Finish CRUD methods

--- noteapp/src/main/java/com/odas/noteapp/controllers/NoteController.java
+++ noteapp/src/main/java/com/odas/noteapp/controllers/NoteController.java
@@ -1,9 +1,10 @@
 package com.odas.noteapp.controllers;
 
 import com.odas.noteapp.services.NoteService;
+import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Set;
 
@@ -13,8 +14,28 @@ public class NoteController {
     @Autowired
     NoteService noteService;
 
+    @PostMapping("/api/upload")
+    public long uploadNote(@RequestParam("note")MultipartFile noteFile){
+        return noteService.uploadNote(noteFile);
+    }
+
     @GetMapping("/api/get")
     public Set<String> fetchNoteList(){
         return noteService.findAll();
+    }
+
+    @GetMapping(value = "api/get/{noteId}", produces = "text/plain")
+    public Resource serveNote(@PathVariable long noteId){
+        return noteService.serveNote(noteId);
+    }
+
+    @PostMapping("/api/update/{noteId}")
+    public void updateNote(@PathVariable long noteId, @RequestParam("note") MultipartFile updatedNote){
+        noteService.updateNote(noteId, updatedNote);
+    }
+
+    @DeleteMapping("/api/delete/{noteId}")
+    public void deleteNote(@PathVariable long noteId){
+        noteService.deleteNote(noteId);
     }
 }
