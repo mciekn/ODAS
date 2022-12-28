838cbc24243e69c0484ef4c15c58c01ce50a2781
Maciej Niszczota <macni.macni@gmail.com>
2022-12-28 17:00:12 +0100

    [ADD POSTGRESQL] Add setup scripts, enhance dockerfiles & configurations.

---
+++ setup_scripts/setup.ps1
@@ -0,0 +1,3 @@
+echo "Generating correct enviromental variables ..."
+echo "TARGET_PLATFORM=windows/amd64" > .env
+echo "Done. Your platform is windows."
---
+++ setup_scripts/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Generating correct enviromental variables ..."
+echo "TARGET_PLATFORM=linux/amd64" > .env
+echo "Done. Your platform is amd/linux."
+
